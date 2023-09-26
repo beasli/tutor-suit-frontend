f9de68c4caa3fe867293df2cf266ddd9ea69e4ea
beasli <shubhamsharmass007@gmail.com>
2023-09-27 00:51:12 +0530

    ready

--- src/util/js/extranal-cdn.js
+++ src/util/js/extranal-cdn.js
@@ -10,21 +10,3 @@ document.body.appendChild(script);
 
 
 
-// Create a script element for the external script
-const fbScript = document.createElement('script');
-fbScript.src = 'https://connect.facebook.net/en_US/sdk.js';
-fbScript.async = true;
-fbScript.defer = true;
-fbScript.crossorigin="anonymous";
-
-// Append the script element to the document
-document.body.appendChild(fbScript);
-
-window.fbAsyncInit = function() {
-    FB.init({
-    appId      : FB_APP_ID,
-    cookie     : true,                     // Enable cookies to allow the server to access the session.
-    xfbml      : true,                     // Parse social plugins on this webpage.
-    version    : 'v17.0'           // Use this Graph API version for this call.
-    });
-};
